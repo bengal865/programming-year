441f3a6beb0ca8b32e72697d4dac6c569fb60daf
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-01-18 10:25:26 -0500

    Create python-todo-template.py

---
+++ python-todo-template.py
@@ -0,0 +1,11 @@
+# First and last name
+# Current date
+# Project title
+
+# --- PART 1: SETUP ---
+# TODO: Create a variable called 'username' and assign it a string value
+username = ""
+
+# --- PART 2: LOGIC ---
+# TODO: Use an 'if' statement to check if the username is longer than 5 characters
+# HINT: Use the len() function
